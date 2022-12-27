bb463b459301bf7e713dec74c32fcb82650d7f61
two.pizzas <54746390+Bernhard90@users.noreply.github.com>
2022-12-27 10:29:10 +0100

    refactoring #2

---
+++ config.py
@@ -0,0 +1,10 @@
+params = {
+    "iterations": 100,
+    "algorithm": "monte-carlo",
+    "logging": False,
+    "model": {
+        "epsilon": 0.4,
+        "step_size": 0.2,
+        "pre_trained": True,
+    }
+}
--- run.py
+++ run.py
@@ -2,28 +2,15 @@ import pandas as pd
 import numpy as np
 
 import src.environment as uno
-
+import config as conf
 
 def main():
-    
-    winners, turns, coverage = list(), list(), list()
-
-    params = {
-        "iterations": 100,
-        "algorithm": "monte-carlo",
-        "logging": False,
-        "model": {
-            "epsilon": 0.4,
-            "step_size": 0.2,
-            "pre_trained": True,
-        }
-    }
 
     run = uno.tournament(
-        iterations = params['iterations'],
-        algo       = params['algorithm'],
-        comment    = params['logging'],
-        agent_info = params['model']
+        iterations = conf.params['iterations'],
+        algo       = conf.params['algorithm'],
+        comment    = conf.params['logging'],
+        agent_info = conf.params['model']
     )
 
     result = pd.concat([
@@ -34,8 +21,12 @@ def main():
     result["win_rate"] = np.where(result["winner"]=="Bernhard",1,0)
     result["win_rate"] = result["win_rate"].cumsum()/(result.index+1)
 
-    result.to_csv("assets/results.csv")
-    print('done')
+    q_vals = pd.DataFrame(uno.agent.q)
+    q_vals.index.rename("id", inplace=True)
+
+    q_vals.to_csv("assets/q-values.csv", index=True)
+    result.to_csv("assets/results.csv", index=False) 
+    
 
 if __name__ == "__main__":
     main()
