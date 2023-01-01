415ab94b40dac3a5d7950d577d1bcb682c9e47c2
Bernhard Pfann <bernhard.pfann@gmail.com>
2023-01-01 17:00:21 +0100

    moved step function to parent class

--- src/agents.py
+++ src/agents.py
@@ -23,97 +23,7 @@ class Agent(object):
         
         self.visit = self.q.copy()
 
-
-class QLearningAgent(Agent):
-    
-    def __init__(self, agent_info:dict):        
-        
-        super().__init__(agent_info)
-        self.prev_state  = 0
-        self.prev_action = 0
-    
     def step(self, state_dict:dict, actions_dict:dict):
-        """
-        Choose the optimal next action according to the followed policy.
-        Required parameters:
-            - state_dict as dict
-            - actions_dict as dict
-        """
-        
-        # (1) Transform state dictionary into tuple
-        state = [i for i in state_dict.values()]
-        state = tuple(state)
-        
-        # (2) Choose action using epsilon greedy
-        # (2a) Random action
-        if random.random() < self.epsilon:
-            
-            actions_possible = [key for key,val in actions_dict.items() if val != 0]         
-            action = random.choice(actions_possible)
-        
-        # (2b) Greedy action
-        else:
-            actions_possible = [key for key,val in actions_dict.items() if val != 0]
-            random.shuffle(actions_possible)
-            val_max = 0
-            
-            for i in actions_possible:
-                val = self.q.loc[[state],i][0]
-                if val >= val_max: 
-                    val_max = val
-                    action = i
-        
-        return action
-    
-    
-    def update(self, state_dict, action):
-        """
-        Updating Q-values according to Belman equation
-        Required parameters:
-            - state_dict as dict
-            - action as str
-        """
-        state = [i for i in state_dict.values()]
-        state = tuple(state)
-        
-        # (1) Set prev_state unless first turn
-        if self.prev_state != 0:
-            prev_q = self.q.loc[[self.prev_state], self.prev_action][0]
-            this_q = self.q.loc[[state], action][0]
-            reward = self.R.loc[[state], action][0]
-            
-            print ("\n")
-            print (f'prev_q: {prev_q}')
-            print (f'this_q: {this_q}')
-            print (f'prev_state: {self.prev_state}')
-            print (f'this_state: {state}')
-            print (f'prev_action: {self.prev_action}')
-            print (f'this_action: {action}')
-            print (f'reward: {reward}')
-            
-            # Calculate new Q-values
-            if reward == 0:
-                self.q.loc[[self.prev_state], self.prev_action] = prev_q + self.step_size * (reward + this_q - prev_q) 
-            else:
-                self.q.loc[[self.prev_state], self.prev_action] = prev_q + self.step_size * (reward - prev_q)
-                
-            self.visit.loc[[self.prev_state], self.prev_action] += 1
-            
-        # (2) Save and return action/state
-        self.prev_state  = state
-        self.prev_action = action
-      
-        
-class MonteCarloAgent(Agent):
-
-    def __init__(self, agent_info):
-
-        super().__init__(agent_info)
-        self.state_seen  = list()
-        self.action_seen = list()
-        self.q_seen      = list()
-           
-    def step(self, state_dict, actions_dict):
         """
         Choose the optimal next action according to the followed policy.
         Required parameters:
@@ -153,9 +63,66 @@ class MonteCarloAgent(Agent):
         self.visit.loc[[state], action] += 1
         
         return action
+
+
+class QLearningAgent(Agent):
     
+    def __init__(self, agent_info:dict):        
+        
+        super().__init__(agent_info)
+        self.prev_state  = 0
+        self.prev_action = 0
     
-    def update(self, state_dict, action):
+    def update(self, state_dict:dict, action):
+        """
+        Updating Q-values according to Belman equation
+        Required parameters:
+            - state_dict as dict
+            - action as str
+        """
+
+        import pdb; pdb.set_trace()
+        state = [i for i in state_dict.values()]
+        state = tuple(state)
+        
+        # (1) Set prev_state unless first turn
+        if self.prev_state != 0:
+            prev_q = self.q.loc[[self.prev_state], self.prev_action][0]
+            this_q = self.q.loc[[state], action][0]
+            reward = self.R.loc[[state], action][0]
+            
+            print ("\n")
+            print (f'prev_q: {prev_q}')
+            print (f'this_q: {this_q}')
+            print (f'prev_state: {self.prev_state}')
+            print (f'this_state: {state}')
+            print (f'prev_action: {self.prev_action}')
+            print (f'this_action: {action}')
+            print (f'reward: {reward}')
+            
+            # Calculate new Q-values
+            if reward == 0:
+                self.q.loc[[self.prev_state], self.prev_action] = prev_q + self.step_size * (reward + this_q - prev_q) 
+            else:
+                self.q.loc[[self.prev_state], self.prev_action] = prev_q + self.step_size * (reward - prev_q)
+                
+            self.visit.loc[[self.prev_state], self.prev_action] += 1
+            
+        # (2) Save and return action/state
+        self.prev_state  = state
+        self.prev_action = action
+      
+        
+class MonteCarloAgent(Agent):
+
+    def __init__(self, agent_info:dict):
+
+        super().__init__(agent_info)
+        self.state_seen  = list()
+        self.action_seen = list()
+        self.q_seen      = list()
+    
+    def update(self, state_dict:dict, action):
         """
         Updating Q-values according to Belman equation
         Required parameters:
